da3791b90b4abdddc349b056cf47c6a8bc88dbba
Demonity11 <xxdm_11@outlook.com>
2026-06-06 15:17:44 -0300

    orbital camera implemented.

--- main.cpp
+++ main.cpp
@@ -8,17 +8,25 @@
 #include <vector>
 
 void processInput(GLFWwindow* window);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+void mouse_cursor_callback(GLFWwindow* window, double xpos, double ypos);
 void vertexSpec();
 
 unsigned int VBO{};
 unsigned int VAO{};
 
-glm::vec3 cameraPos   { 0.0f, 0.0f,  3.0f };
-glm::vec3 cameraTarget{ 0.0f, 0.0f, -1.0f };
+glm::vec3 cameraPos   { 0.0f, 0.5f,  3.0f };
+glm::vec3 cameraTarget{ 0.0f, 0.0f, 0.0f };
 glm::vec3 worldUp     { 0.0f, 1.0f,  0.0f };
 
-float deltaTime{ 0.0f };
-float lastTime{ 0.0f };
+float lastX{ 400.0f };
+float lastY{ 300.0f };
+
+float yaw{};
+float pitch{};
+
+bool isPressingRightClick{ false };
+bool isFirstMouse{ true };
 
 int main()
 {
@@ -31,21 +39,23 @@ int main()
 
 	Shader shader{ "shader.vert", "shader.frag" };
 
+	glfwSetMouseButtonCallback(window.getWindow(), mouse_button_callback);
+	glfwSetCursorPosCallback(window.getWindow(), mouse_cursor_callback);
+
 	vertexSpec();
 
 	while (!glfwWindowShouldClose(window.getWindow()))
 	{
 		processInput(window.getWindow());
 
-		window.clear(1.0f, 0.0f, 0.0f, 1.0f);
+		window.clear(0.6f, 0.3f, 0.0f, 1.0f);
 
 		shader.use();
 
-		glm::mat4 view{ glm::lookAt(cameraPos, cameraPos + cameraTarget, worldUp) };
+		glm::mat4 view{ glm::lookAt(cameraPos, cameraTarget, worldUp) };
 
-		float currentFrame{ static_cast<float>(glfwGetTime()) };
-		deltaTime = currentFrame - lastTime;
-		lastTime = currentFrame;
+		//view = glm::rotate(view, yaw, glm::vec3(0.0f, 1.0f, 0.0f));
+		//view = glm::rotate(view, pitch, glm::vec3(1.0f, 0.0f, 0.0f));
 
 		glm::mat4 projection{};
 		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
@@ -53,13 +63,14 @@ int main()
 		glm::mat4 model{ 1.0f };
 
 		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(3.0f, 3.0f, 3.0f));
 
 		shader.setMat4("view", view);
 		shader.setMat4("projection", projection);
 		shader.setMat4("model", model);
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_LINES, 0, 2);
+		glDrawArrays(GL_LINES, 0, 6);
 
 		glfwSwapBuffers(window.getWindow());
 		glfwPollEvents();
@@ -72,12 +83,74 @@ void processInput(GLFWwindow* window)
 		glfwSetWindowShouldClose(window, true);
 }
 
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_RIGHT)
+	{
+		if (action == GLFW_PRESS)
+		{
+			isPressingRightClick = true;
+			isFirstMouse = true;
+		}
+
+		if (action == GLFW_RELEASE)
+			isPressingRightClick = false;
+	}
+}
+
+void mouse_cursor_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (!isPressingRightClick)
+		return;
+
+	if (isFirstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		isFirstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = ypos - lastY;
+
+	lastX = xpos;
+	lastY = ypos;
+
+	const float sensitivity{ 0.01f };
+
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	const float radius{ 2.0f };
+
+	cameraPos.x = cameraTarget.x + radius * cos(yaw) * cos(pitch);
+	cameraPos.y = cameraTarget.y + radius * sin(pitch);
+	cameraPos.z = cameraTarget.z + radius * sin(yaw) * cos(pitch);
+
+	std::cout << "x = " << cameraPos.x << ", y = " << cameraPos.y << ", z = " << cameraPos.z << "\n";
+}
+
 void vertexSpec()
 {
 	std::vector vertices
 	{
 		-1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-		 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f
+		 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+
+		 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+
+		 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f,
+		 0.0f, 0.0f,  1.0f, 1.0f, 0.0f, 0.0f
 	};
 
 	glGenVertexArrays(1, &VAO);
